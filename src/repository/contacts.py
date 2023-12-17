ab6156ce542ba32d0362467bae7533ce0adc8637
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2023-12-17 13:07:33 +0100

    updated functions for contacts

--- src/repository/contacts.py
+++ src/repository/contacts.py
@@ -4,23 +4,27 @@ from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import or_, and_, extract, cast, Date
+from sqlalchemy import or_, and_, extract
 
 from src.entity.models import Contact
 from src.schema.contact import ContactSchema, ContactUpdate
 
 
 async def get_contacts(limit: int, offset: int, name: str, surname: str, email: str, db: AsyncSession):
-    statement = select(Contact).offset(offset).limit(limit)
-    if name:
-        statement = statement.filter(Contact.name.ilike(f"%{name}%"))
-    if surname:
-        statement = statement.filter(Contact.surname.ilike(f"%{surname}%"))
-    if email:
-        statement = statement.filter(Contact.email.ilike(f"%{email}%"))
+    query = select(Contact).offset(offset).limit(limit)
 
-    contacts = await db.execute(statement)
-    await db.close()
+    filters = []
+    if name:
+        filters.append(Contact.name.ilike(f"%{name}%"))
+    if surname:
+        filters.append(Contact.surname.ilike(f"%{surname}%"))
+    if email:
+        filters.append(Contact.email.ilike(f"%{email}%"))
+
+    if filters:
+        query = query.where(and_(*filters))
+
+    contacts = await db.execute(query)
     return contacts.scalars().all()
 
 
@@ -32,6 +36,9 @@ async def get_contact(contact_id: int, db: AsyncSession):
 
 
 async def create_contact(body: ContactSchema, db: AsyncSession):
+    if body.birthday >= date.today():
+        raise HTTPException(status_code=400, detail="Birthday must be in the past")
+
     try:
         contact = Contact(**body.model_dump())
         db.add(contact)
@@ -45,46 +52,69 @@ async def create_contact(body: ContactSchema, db: AsyncSession):
         await db.close()
 
 
-async def update_contact(contact_id: int, contact: ContactUpdate, db: AsyncSession):
-    statement = select(Contact).filter_by(id=contact_id)
-    existing_contact = await db.execute(statement)
-    existing_contact = existing_contact.scalar_one_or_none()
-    if existing_contact:
-        for key, value in contact.model_dump().items():
-            setattr(existing_contact, key, value)
+async def update_contact(contact_id: int, contact_update: ContactUpdate, db: AsyncSession):
+    query = select(Contact).filter_by(id=contact_id)
+    result = await db.execute(query)
+    existing_contact = result.scalar_one_or_none()
+
+    if not existing_contact:
+        raise HTTPException(status_code=404, detail="Contact not found")
+
+    for key, value in contact_update.model_dump(exclude_unset=True).items():
+        setattr(existing_contact, key, value)
+
+    try:
         await db.commit()
         await db.refresh(existing_contact)
-        await db.close()
-    return existing_contact
+        return existing_contact
+    except SQLAlchemyError as e:
+        await db.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 async def delete_contact(contact_id: int, db: AsyncSession):
-    statement = select(Contact).filter_by(id=contact_id)
-    contact = await db.execute(statement)
-    contact = contact.scalar_one_or_none()
-    if contact:
-        await db.delete(contact)
+    query = select(Contact).filter_by(id=contact_id)
+    result = await db.execute(query)
+    contact = result.scalar_one_or_none()
+
+    if not contact:
+        raise HTTPException(status_code=404, detail="Contact not found")
+
+    await db.delete(contact)
+    try:
         await db.commit()
-        return contact
+    except SQLAlchemyError as e:
+        await db.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 async def get_upcoming_birthdays(db: AsyncSession):
     today = date.today()
     next_week = today + timedelta(days=7)
 
-    statement = select(Contact).filter(
+    query = select(Contact).filter(
         or_(
             and_(
-                extract('month', cast(Contact.birthday, Date)) == extract('month', cast(today, Date)),
-                extract('day', cast(Contact.birthday, Date)) >= extract('day', cast(today, Date))
+                extract('month', Contact.birthday) == today.month,
+                extract('day', Contact.birthday) >= today.day,
+                extract('month', Contact.birthday) == next_week.month,
+                extract('day', Contact.birthday) <= next_week.day,
             ),
             and_(
-                extract('month', cast(Contact.birthday, Date)) == extract('month', cast(next_week, Date)),
-                extract('day', cast(Contact.birthday, Date)) <= extract('day', cast(next_week, Date))
+                today.month != next_week.month,
+                or_(
+                    and_(
+                        extract('month', Contact.birthday) == today.month,
+                        extract('day', Contact.birthday) >= today.day,
+                    ),
+                    and_(
+                        extract('month', Contact.birthday) == next_week.month,
+                        extract('day', Contact.birthday) <= next_week.day,
+                    )
+                )
             )
         )
     )
 
-    contacts = await db.execute(statement)
-    await db.close()
-    return contacts.scalars().all()
+    result = await db.execute(query)
+    return result.scalars().all()
