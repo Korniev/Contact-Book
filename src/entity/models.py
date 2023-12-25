4e0d55ad0f61c7978956fdb3c5ee390c9523430d
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2023-12-25 23:36:03 +0100

    updated models

--- src/entity/models.py
+++ src/entity/models.py
@@ -1,7 +1,7 @@
 from datetime import date
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Date
-from sqlalchemy.orm import DeclarativeBase
+
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
+from sqlalchemy import String, Date, Integer, ForeignKey, DateTime, func
 
 
 class Base(DeclarativeBase):
@@ -16,3 +16,23 @@ class Contact(Base):
     email: Mapped[str] = mapped_column(unique=True)
     phone: Mapped[str] = mapped_column()
     birthday: Mapped[date] = mapped_column(Date)
+
+    created_at: Mapped[date] = mapped_column('created_at', DateTime, default=func.now(), nullable=True)
+    updated_at: Mapped[date] = mapped_column('updated_at', DateTime, default=func.now(), onupdate=func.now(),
+                                             nullable=True)
+
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), nullable=True)
+    user: Mapped["User"] = relationship("User", backref='contacts', lazy='joined')
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String(150))
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    avatar: Mapped[str] = mapped_column(String(255), nullable=True)
+    refresh_token: Mapped[str] = mapped_column(String(255), nullable=True)
+
+    created_at: Mapped[date] = mapped_column('created_at', DateTime, default=func.now())
+    updated_at: Mapped[date] = mapped_column('updated_at', DateTime, default=func.now(), onupdate=func.now())
