be0cb341b7eaddd13ba92a6e8e558fa7a26cf382
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2023-12-26 01:41:20 +0100

    updated schema for contacts

--- src/schema/contact.py
+++ src/schema/contact.py
@@ -1,32 +1,37 @@
-from datetime import date
+from datetime import date, datetime
 from typing import Optional
 
 from pydantic import BaseModel, Field, EmailStr
+
+from src.schema.user import UserResponse
 
 
 class ContactSchema(BaseModel):
     name: str = Field(max_length=150)
     surname: str = Field(max_length=150)
     email: EmailStr
-    phone: str = Field(..., pattern=r"^\+?1?\d{9,15}$")
+    phone: str
     birthday: date
 
 
 class ContactUpdate(BaseModel):
-    name: Optional[str] = Field(max_length=150)
-    surname: Optional[str] = Field(max_length=150)
-    email: Optional[EmailStr]
-    phone: Optional[str] = Field(None, pattern=r"^\+?1?\d{9,15}$")
-    birthday: Optional[date]
-
-
-class ContactResponse(BaseModel):
-    id: int
     name: str
     surname: str
     email: EmailStr
     phone: str
     birthday: date
+
+
+class ContactResponse(BaseModel):
+    id: int = 1
+    name: str
+    surname: str
+    email: EmailStr
+    phone: str
+    birthday: date
+    created_at: datetime | None
+    updated_at: datetime | None
+    user: UserResponse | None
 
     class Config:
         from_attributes = True
