0f50d579676bc0bb930f51528c88fd755358e36e
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2023-12-31 17:44:35 +0100

    updated users

---
+++ src/routes/users.py
@@ -0,0 +1,34 @@
+import pickle
+
+import cloudinary
+import cloudinary.uploader
+from fastapi import (
+    APIRouter,
+    HTTPException,
+    Depends,
+    status,
+    Path,
+    Query,
+    UploadFile,
+    File,
+)
+from fastapi_limiter.depends import RateLimiter
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database.db import get_db
+from src.entity.models import User
+from src.schema.user import UserResponse
+from src.services.auth import auth_service
+from src.conf.config import config
+from src.repository import users as repositories_users
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+
+@router.get(
+    "/me",
+    response_model=UserResponse,
+    dependencies=[Depends(RateLimiter(times=1, seconds=20))],
+)
+async def get_current_user(user: User = Depends(auth_service.get_current_user)):
+    return user
