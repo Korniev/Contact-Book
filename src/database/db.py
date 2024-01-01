119933a32dedc857d645e44980b5d5722eb28124
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-01 19:22:25 +0100

    upd documentation for db

--- src/database/db.py
+++ src/database/db.py
@@ -6,6 +6,22 @@ from src.conf.config import config
 
 
 class DatabaseSessionManager:
+    """
+        A class for managing database sessions in an asynchronous context using SQLAlchemy.
+
+        This session manager creates and handles SQLAlchemy sessions for asynchronous database queries.
+        It utilizes a context manager for safely opening and closing sessions.
+
+        Attributes:
+            _engine (AsyncEngine): An asynchronous engine for connecting to the database.
+            _session_maker (async_sessionmaker): A factory for creating new asynchronous sessions.
+
+        Args:
+            url (str): The database connection URL.
+
+        Methods:
+            session: A context manager for creating and managing database sessions.
+    """
     def __init__(self, url: str):
         self._engine: AsyncEngine | None = create_async_engine(url)
         self._session_maker: async_sessionmaker = async_sessionmaker(autoflush=False, autocommit=False,
@@ -29,5 +45,15 @@ sessionmanager = DatabaseSessionManager(config.DB_URL)
 
 
 async def get_db():
+    """
+        An asynchronous generator that provides access to the database session.
+
+        This generator is used in FastAPI routes to obtain and manage database sessions.
+        It employs the `DatabaseSessionManager` for creating an asynchronous session,
+        ensuring proper session opening and closure.
+
+        Yields:
+            AsyncSession: An asynchronous database session for use in API routes.
+        """
     async with sessionmanager.session() as session:
         yield session
