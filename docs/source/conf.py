01745f982ccdf7dc98def8ff7dd92f09ea7b3329
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-01 11:28:08 +0100

    updated file conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,7 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import sys
+import os
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+sys.path.append(os.path.abspath('..'))
 
 project = 'ContactBook'
 copyright = '2024, Yevhenii Korniev'
@@ -14,15 +11,15 @@ release = '2024'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 language = 'En'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'nature'
 html_static_path = ['_static']
