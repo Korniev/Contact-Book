6a7dbf8f37990ac1c3eea9543177e66c71a98328
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-02 18:57:56 +0100

    tests for route auth

---
+++ tests/test_routes_auth.py
@@ -0,0 +1,62 @@
+from unittest import mock
+from unittest.mock import Mock
+
+import pytest
+from src.conf import messages as msg
+from fastapi import status
+
+new_test_user = {
+    "username": "test_user",
+    "email": "test_user@example.com",
+    "password": "qwerty22"
+}
+
+real_user = {
+    "username": "korniev",
+    "email": "korniev92@gmail.com",
+    "password": "567234"
+}
+
+
+def test_signup(client, monkeypatch):
+    mock_send_email = Mock()
+    monkeypatch.setattr("src.routes.auth.send_email", mock_send_email)
+
+    response = client.post("api/auth/signup", json=new_test_user)
+    assert response.status_code == status.HTTP_201_CREATED, response.text
+    data = response.json()
+    assert data["username"] == new_test_user["username"]
+    assert data["email"] == new_test_user["email"]
+    assert new_test_user["password"] not in data
+    assert "avatar" in data
+
+
+def test_signup_with_existing_email(client, monkeypatch):
+    mock_send_email = Mock()
+    monkeypatch.setattr("src.routes.auth.send_email", mock_send_email)
+    response = client.post("api/auth/signup", json=real_user)
+    assert response.status_code == status.HTTP_201_CREATED, response.text
+    assert response.json() == {"detail": msg.ACCOUNT_EXISTS}
+
+
+def test_login_with_out_confirmed(client):
+    response = client.post("api/auth/login",
+                           data={"username": new_test_user["email"], "password": new_test_user["password"]})
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED, response.text
+    assert response.json() == {"detail": msg.ACCOUNT_NOT_CONFIRMED}
+
+
+def test_request_email(client, monkeypatch):
+    mock_send_email = Mock()
+    monkeypatch.setattr("src.routes.auth.send_email", mock_send_email)
+
+    response = client.post("/api/auth/request_email", json=new_test_user)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"message": "Check your email for confirmation."}
+
+
+def test_request_email_open(client):
+    username = "new_test_user"
+    response = client.get(f"/api/auth/{username}")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.headers["content-type"] == "image/png"
